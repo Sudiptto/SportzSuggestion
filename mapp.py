7c852dbdb9c2fda5744e6f0283fb37414256a1f6
Sudiptto Biswas <biswassudiptto@gmail.com>
2023-06-11 18:12:44 -0400

    finished

--- mapp.py
+++ mapp.py
@@ -6,28 +6,37 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_PASSWORD')
 
-
-#lat = None
-
-
 @app.route('/info', methods=['POST'])
 def info():
     if request.method == 'POST':
         req = request.get_json()
         print(req)
-        # create a global lat variable to be used
-        #global lat
         lat = req['lat']
         long = req['longitude']
-        print(lat, long)
-        worked = "It worked!"
+        #print(lat, long), we know that this is working!
 
-    # Return the worked variable as a JSON response
+        promt = f'Based on these coordinates give me the name of the city and only the name of the city: Latitude: {lat}, Longitude: {long}. Do not do a sentence ONLY the name of the city and no sentence'
+        city = find_city(promt)
+        #print(user_city) - user_city prompt works!
+        weather_info = weather_data(city) # use this to get information about weather
+        if len(weather_info) == 5:
+            humidity = weather_info[0]
+            temperature = weather_info[1]
+            percipitation = weather_info[2]
+            cloud_cover = weather_info[3]
+            wind_speed = weather_info[4]
 
-    # NOTE FOR TOMORROW OR LATER TODAY THE WORKED SENT!
-        return jsonify({'worked': worked})
+            ai_suggestion = user_inputs(f"Name one unique sport that you can play when the temperature is {temperature}, when the humidity is {humidity}% and when the percipitation is level is {percipitation}% and when the percentage of clouds in the sky is {cloud_cover} and finally when the wind speed is {wind_speed} miles per hour")
+            suggest = f'City Found: {city} and sport suggested: ' + ai_suggestion
+            return jsonify({'suggest': suggest}) # SUCCESS THIS WORKED AND RETURNED IT CORRECTLY 
+            """else: 
+            return jsonify({'Issue': issue}) 
+            Note: Don't need an else statement to catch error as in the javascript it will catch the error
+            """
+        #return jsonify({'worked': worked})
     
-
+    
+    # FOR TOMORROW USE THE OPENAI API TO GET THE CITY NAME BASED ON THE INFORMATION WE GOT 
     #return render_template('home.html', lat=None)
 
 
